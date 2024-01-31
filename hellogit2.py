b3f51949ad563c031f8fef12cd64ba8ee3f90a98
Rolandoors <Rolandoors1@yahoo.com>
2024-01-31 22:32:25 +0000

    Primer Commit: Inicio del proyecto

---
+++ hellogit2.py
@@ -0,0 +1,12 @@
+import math
+
+print("Hello git dos")
+
+numero = 1500033
+digitos = 0
+print("Digitos 2:", int(math.log10(numero)+1))
+
+digitos = str(numero)
+
+print("Digitos: " ,len(digitos))
+
---
+++ login.py
@@ -0,0 +1,15 @@
+<<<<<<< Updated upstream
+print("Login, Ver.3")
+=======
+print("Login, Ver.3")
+
+relativo = int(input("Ingresar ralativo: " ))
+
+
+def funcionPrincipal(relativo) ->  int:
+    resultado = relativo ** 3
+    return relativo
+
+funcionPrincipal(relativo)
+
+>>>>>>> Stashed changes
