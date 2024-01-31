55010c8dfc4321649302098381c2033c03c50ebc
Rolando Roldán Santos <105856971+Cabalista@users.noreply.github.com>
2024-01-31 23:34:49 +0000

    Creación, Clase Usuario

--- hellogit4.py
+++ hellogit4.py
@@ -1,1 +1,14 @@
+import time 
 
+class Usuarios:
+  def __init__(self, nombre, apellidos, edad):
+    self.nombre = nombre
+    self.apellidos = apellidos
+    self.edad = edad
+
+  def setNombre(self, nombre):
+    self.nombre = nombre
+
+  def getNombre(self):
+    print(self.nombre)
+  
